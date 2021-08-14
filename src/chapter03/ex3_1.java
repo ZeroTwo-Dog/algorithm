f6ea798b1e0c593ba4c48372ab7753dda30ac6e7
JiHwan Park <88587913+ZeroTwo-Dog@users.noreply.github.com>
2021-08-14 13:42:37 +0900

    알고리즘 3_1

---
+++ src/chapter03/ex3_1.java
@@ -0,0 +1,29 @@
+package chapter03;
+
+import java.util.Scanner;
+
+
+/**
+ * Created by park
+ * 2021-08-14
+ */
+public class ex3_1 {
+
+  public static void main(String[] args) {
+    //잔돈입력
+    Scanner sc = new Scanner(System.in);
+    int data = sc.nextInt();
+    //변수세팅
+    //[] 배열 초기화 {}
+    int[] coins = {500, 100, 50, 10};
+    int count = 0;
+    for (int coin : coins) {
+      count += data / coin;
+      data = data % coin;
+
+
+    }
+    System.out.println("총 동전 갯수: : " + count);
+
+  }
+}
