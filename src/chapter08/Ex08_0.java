5dfab989490412951c7725496b66a11fdd918903
zeroTwo <88587913+ZeroTwo-Dog@users.noreply.github.com>
2021-10-06 22:15:25 +0900

    Feat: 파노마치수열 (다이나믹프로그래밍)
    - 정적 코드진행 확인
    
    EX8_0: #완료

---
+++ src/chapter08/Ex08_0.java
@@ -0,0 +1,26 @@
+package chapter08;
+
+/**
+ * 파보나치 수열
+ * Created by park on 2021/10/06.
+ */
+public class Ex08_0 {
+
+  final static int[] arr = new int[99];
+  public static int pibo(int x) {
+    if(x == 1 || x == 2) {
+      return 1;
+    }
+    if (arr[x] != 0) {
+      return arr[x];
+    }
+    // 정적으로 코드를 실행 하기때문에 앞에 pibo함수를 x=1까지 실행하고 뒤에 pibo를 실행함
+    arr[x] = pibo(x-1) + pibo(x-2);
+    return arr[x];
+  }
+  public static void main(String[] args) {
+
+    System.out.println(pibo(6));
+  }
+
+}
